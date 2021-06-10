bbcf06453fbd4a3f398a75bb54b2b2c4eb3d9d3f
Areddy28 <reddyanurag28@gmail.com>
2021-06-10 09:34:39 -0400

    Loop Exercise 1

---
+++ scripts.js
@@ -0,0 +1,44 @@
+for(var i=1; i<11; i++){
+    console.log(i) 
+}    
+
+for (var j= 10; j>=1; j--) {
+    console.log(j)   
+}
+
+
+let k = 1; 
+while (k < 11) {
+    console.log(k)
+    k++; 
+}
+
+let h = 10; 
+while (h >=1) {
+    console.log(h)
+    h--; 
+}
+
+let a = 1; 
+
+do {
+    console.log(a)
+        a++; 
+}
+
+while (a<11) 
+
+let b = 10
+
+do {
+    console.log(b)
+        b--; 
+}
+
+while (b>=1)  
+
+const array1 = [1,2,3,4,5,6,7,8,9,10] 
+
+for (const element of array1) {
+    console.log(element); 
+}
