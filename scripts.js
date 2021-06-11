e2fe0579a8448d866f05b28ef322846987482565
Areddy28 <reddyanurag28@gmail.com>
2021-06-11 08:44:39 -0400

    Loop Exercise 1

--- scripts.js
+++ scripts.js
@@ -6,6 +6,8 @@ for (var j= 10; j>=1; j--) {
     console.log(j)   
 }
 
+console.log("Above are my for loops"); 
+
 
 let k = 1; 
 while (k < 11) {
@@ -18,6 +20,8 @@ while (h >=1) {
     console.log(h)
     h--; 
 }
+
+console.log("Above are my while loops"); 
 
 let a = 1; 
 
@@ -37,8 +41,12 @@ do {
 
 while (b>=1)  
 
+console.log("Above are my do while loops"); 
+
 const array1 = [1,2,3,4,5,6,7,8,9,10] 
 
 for (const element of array1) {
     console.log(element); 
 }
+
+console.log("Above is my Array"); 
